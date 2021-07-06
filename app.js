5623979665200c7a260d5a5937259f4208414c9d
D'Angela Pritchett-Rowe <dangela.prit.rowe@gmail.com>
2021-07-06 13:52:02 -0400

    Refactoring

--- app.js
+++ app.js
@@ -1,25 +1,6 @@
-
-setTimeout( () => {
-    document.body.style.backgroundColor = 'red';
-    setTimeout( () => {
-        document.body.style.backgroundColor = 'orange';
-        setTimeout( () => {
-            document.body.style.backgroundColor = 'yellow';
-            setTimeout( () => {
-                document.body.style.backgroundColor = 'green';
-                setTimeout( () => {
-                    document.body.style.backgroundColor = 'blue';
-                    setTimeout( () => {
-                        document.body.style.backgroundColor = 'indigo';
-                        setTimeout( () => {
-                            document.body.style.backgroundColor = 'violet';
-                        }, 1000)
-                    }, 1000)
-                }, 1000)
-            }, 1000)
-        }, 1000)
-    }, 1000)
-}, 1000)
+// setTimeout( () => {
+//     document.body.style.backgroundColor = 'red';
+// }, 1000)
 
 // setTimeout( () => {
 //     document.body.style.backgroundColor = 'orange';
@@ -45,8 +26,50 @@ setTimeout( () => {
 //     document.body.style.backgroundColor = 'violet';
 // }, 7000)
 
-const delayedColorChange = (newColor, delay) => {
+
+// setTimeout( () => {
+    //     document.body.style.backgroundColor = 'red';
+    //     setTimeout( () => {
+    //         document.body.style.backgroundColor = 'orange';
+    //         setTimeout( () => {
+    //             document.body.style.backgroundColor = 'yellow';
+    //             setTimeout( () => {
+    //                 document.body.style.backgroundColor = 'green';
+    //                 setTimeout( () => {
+    //                     document.body.style.backgroundColor = 'blue';
+    //                     setTimeout( () => {
+    //                         document.body.style.backgroundColor = 'indigo';
+    //                         setTimeout( () => {
+    //                             document.body.style.backgroundColor = 'violet';
+    //                         }, 1000)
+    //                     }, 1000)
+    //                 }, 1000)
+    //             }, 1000)
+    //         }, 1000)
+    //     }, 1000)
+    // }, 1000)
+
+
+const delayedColorChange = (newColor, delay, doNext) => {
     setTimeout(() => {
         document.body.style.backgroundColor = newColor;
+        doNext && doNext();
     }, delay)
 }
+
+
+delayedColorChange('red', 1000, () => {
+    delayedColorChange('orange', 1000, () => {
+        delayedColorChange('yellow', 1000, () => {
+            delayedColorChange('green', 1000, () => {
+                delayedColorChange('blue', 1000, () => {
+                    delayedColorChange('indigo', 1000, () => {
+                        delayedColorChange('violet', 1000, () => {
+
+                        })
+                    })
+                })
+            })
+        })
+    })
+})
