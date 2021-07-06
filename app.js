ba14b56ef40d0f46fa3b02a0dc4cfd0b620bee25
D'Angela Pritchett-Rowe <dangela.prit.rowe@gmail.com>
2021-07-06 13:33:55 -0400

    Background color changes to ROYGBIV every 1 s

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
 
 setTimeout( () => {
     document.body.style.backgroundColor = 'red';
+}, 1000)
+
+setTimeout( () => {
+    document.body.style.backgroundColor = 'orange';
 }, 2000)
 
-// document.body.style.backgroundColor = 'red';
+setTimeout( () => {
+    document.body.style.backgroundColor = 'yellow';
+}, 3000)
+
+setTimeout( () => {
+    document.body.style.backgroundColor = 'green';
+}, 4000)
+
+setTimeout( () => {
+    document.body.style.backgroundColor = 'blue';
+}, 5000)
+
+setTimeout( () => {
+    document.body.style.backgroundColor = 'indigo';
+}, 6000)
+
+setTimeout( () => {
+    document.body.style.backgroundColor = 'violet';
+}, 7000)
+
