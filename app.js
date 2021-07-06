8d7a6b0a276c21b052f1e6112a17a8b0b5332856
D'Angela Pritchett-Rowe <dangela.prit.rowe@gmail.com>
2021-07-06 13:38:08 -0400

    Same ROYGBIV background color change after 1 s using nested callbacks

--- app.js
+++ app.js
@@ -1,29 +1,47 @@
 
 setTimeout( () => {
     document.body.style.backgroundColor = 'red';
+    setTimeout( () => {
+        document.body.style.backgroundColor = 'orange';
+        setTimeout( () => {
+            document.body.style.backgroundColor = 'yellow';
+            setTimeout( () => {
+                document.body.style.backgroundColor = 'green';
+                setTimeout( () => {
+                    document.body.style.backgroundColor = 'blue';
+                    setTimeout( () => {
+                        document.body.style.backgroundColor = 'indigo';
+                        setTimeout( () => {
+                            document.body.style.backgroundColor = 'violet';
+                        }, 1000)
+                    }, 1000)
+                }, 1000)
+            }, 1000)
+        }, 1000)
+    }, 1000)
 }, 1000)
 
-setTimeout( () => {
-    document.body.style.backgroundColor = 'orange';
-}, 2000)
+// setTimeout( () => {
+//     document.body.style.backgroundColor = 'orange';
+// }, 2000)
 
-setTimeout( () => {
-    document.body.style.backgroundColor = 'yellow';
-}, 3000)
+// setTimeout( () => {
+//     document.body.style.backgroundColor = 'yellow';
+// }, 3000)
 
-setTimeout( () => {
-    document.body.style.backgroundColor = 'green';
-}, 4000)
+// setTimeout( () => {
+//     document.body.style.backgroundColor = 'green';
+// }, 4000)
 
-setTimeout( () => {
-    document.body.style.backgroundColor = 'blue';
-}, 5000)
+// setTimeout( () => {
+//     document.body.style.backgroundColor = 'blue';
+// }, 5000)
 
-setTimeout( () => {
-    document.body.style.backgroundColor = 'indigo';
-}, 6000)
+// setTimeout( () => {
+//     document.body.style.backgroundColor = 'indigo';
+// }, 6000)
 
-setTimeout( () => {
-    document.body.style.backgroundColor = 'violet';
-}, 7000)
+// setTimeout( () => {
+//     document.body.style.backgroundColor = 'violet';
+// }, 7000)
 
